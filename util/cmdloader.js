84d2d4ed79f198ae8bcd99c5f4bb7463f6365ff6
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-03 17:51:36 +0100

    Update and rename cmdloader.js to util/cmdloader.js

---
+++ util/cmdloader.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const { promisify } = require("util");
+const readdir = promisify(require("fs").readdir);
+
+module.exports = async (client) => {
+    client.commands = new Discord.Collection();
+    client.aliases = new Discord.Collection();
+    client.informationCommands = new Discord.Collection();
+    client.miscCommands = new Discord.Collection();
+
+
+const miscFiles = await readdir("./commands/Misc/");
+
+
+
+
+miscFiles.forEach(f => {
+    if (!f.endsWith(".js")) return;
+		let misc = require(`../commands/Misc/${f}`);
+    client.miscCommands.set(misc.help.name, misc);
+    client.commands.set(misc.help.name, misc);
+	client.aliases.set(misc.help.alias, misc);
+	});
+console.log(`loaded ${client.commands.size} commands`);
+console.log(`loaded ${client.aliases.size} aliases`);
+}
