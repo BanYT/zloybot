64f7971dc676fecaf32edfaabcb9f0b348ba8e45
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-03 18:13:23 +0100

    Update dmeveryone.js

--- commands/dmeveryone.js
+++ commands/dmeveryone.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-if(!message.member.hasPermissions("ADMINISTRATOR")) return message.channel.send("you dont have a permisions").then(x=>x.delete(3000))
+if(!message.member.hasPermissions("ADMINISTRATOR")) return message.channel.send("you dont have a permisions");
 message.guild.members.map(x=>x.sendMessage(args.join(" ")))
 
   }
